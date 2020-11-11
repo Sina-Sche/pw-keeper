6dd95ffcf4ca28c3044e41161e915dbbdc46ce58
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-11 10:26:43 +0100

    :sparkles: Add write function
    Add write function

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const args = process.argv.slice(2);
+const passwordName = args[0];
+const newPasswordValue = args[1];
+
+const pwSafeJSON = fs.readFileSync("./db.json", "utf8");
+let pwSafe = JSON.parse(pwSafeJSON);
+let pw = pwSafe[passwordName];
+
 console.log("Welcome to your Password Manager");
-
-const showPassword = () => {
-  const args = process.argv.slice(2);
-  const passwordName = args[0];
-
-  console.log(`Here is your password for your ${passwordName} account`);
-
-  const pwSafeJSON = fs.readFileSync("./db.json", "utf8");
-
-  const pwSafe = JSON.parse(pwSafeJSON);
-  const pw = pwSafe[passwordName];
-  if (passwordName) {
-    console.log(`The password is ${pw}`), process.exit(1);
-    return;
-  } else {
-    console.log("unknown password");
-  }
-};
 const loginPw = "0000";
 const question = [
   {
@@ -28,14 +18,36 @@ const question = [
     message: "Please enter the master password",
     mask: "*",
 
-    validate: async function (value) {
+    validate: function (value) {
       if (value == loginPw) {
-        return "Correct Password", await showPassword();
+        return allowAccess();
       } else {
         console.error("You better leave NOW!👊");
       }
     },
   },
 ];
+const showPassword = () => {
+  if (passwordName) {
+    console.log(`The password is ${pw}`), process.exit(1);
+    return showPassword();
+  } else {
+    console.log("unknown password");
+  }
+};
+const writePassword = () => {
+  pwSafe[passwordName] = newPasswordValue;
+  fs.writeFileSync("./db.json", JSON.stringify(pwSafe, null, 2));
+};
+
+const allowAccess = () => {
+  if (newPasswordValue) {
+    writePassword(),
+      console.log("You have changed the password!"),
+      process.exit(1);
+  } else {
+    showPassword(), process.exit(1);
+  }
+};
 
 inquirer.prompt(question);
